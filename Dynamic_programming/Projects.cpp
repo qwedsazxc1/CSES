8908a6cbfc64386847a8ae1d25989a3d070ab1cd
keep1earning <linyihong1234@gmail.com>
2023-04-06 04:45:54 +0800

    modified Dynamic_programming/Projects.cpp Projects finished

--- Dynamic_programming/Projects.cpp
+++ Dynamic_programming/Projects.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
+#include <map>
+#include <algorithm>
+#include <utility>
+#include <vector>
+using namespace std;
 
-int main(){
+signed main(){
     int n;
-    scanf("%d", &n);
-    
+    cin >> n;
+    vector<pair<pair<int, int>, unsigned long long>> arr(n);
+    for (int i = 0; i < n; i++){
+        cin >> arr[i].first.second >> arr[i].first.first >> arr[i].second;
+    }
+    sort(arr.begin(), arr.end());
+    map<int, unsigned long long> dp;
+    dp[0] = 0;
+    for (int i = 0; i < n; i++){
+        int st = arr[i].first.second, end = arr[i].first.first;
+        unsigned long long w = arr[i].second;
+        auto it = dp.lower_bound(st);
+        it--;
+        unsigned long long prev_w = (*it).second;
+        auto check = dp.upper_bound(end);
+        check--;
+        dp[end] = max(prev_w + w, (*check).second);
+    }
+    auto it = dp.end();
+    it--;
+    cout << (*it).second << "\n";
     return 0;
 }
