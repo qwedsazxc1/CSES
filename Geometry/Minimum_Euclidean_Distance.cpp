4bfd0d8c6412ccd924842a523a021815f9d35dc3
keep1earning <linyihong1234@gmail.com>
2023-06-28 16:23:54 +0800

    new Geometry/Minimum_Euclidean_Distance.cpp Minimum Euclidean Distance finished tag: Geometry, Sort, Binary search

---
+++ Geometry/Minimum_Euclidean_Distance.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#define x real()
+#define y imag()
+#define INF 0x7f3f3f3f3f3f3f3f
+using namespace std;
+typedef complex<long long> P;
+typedef long long ll;
+
+bool cmp(P a, P b){
+	if (a.x == b.x)
+		return a.y < b.y;
+	return a.x < b.x;
+}
+
+struct mcmp{
+	bool operator()(P a, P b) const {
+		if (a.y == b.y)
+			return a.x < b.x;
+		return a.y < b.y;
+	}
+};
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	P arr[n];
+	for (int i = 0; i < n; i++){
+		ll real, imag;
+		cin >> real >> imag;
+		arr[i] = {real, imag};
+	}
+	sort(arr, arr + n, cmp);
+	set<P, mcmp> s;
+	s.insert(arr[0]);
+	ll d = INF;
+	int l = 0;
+	for (int i = 1; i < n; i++){
+		P cur = arr[i];
+		while (l < i){
+			if (arr[l].x >= cur.x - (ll)sqrt(d)) break;
+			s.erase({arr[l++]});
+		}
+
+		auto from = s.lower_bound({cur.x - d, cur.y - d}), to = s.upper_bound({cur.x, cur.y + d});
+		for (; from != to; from++){
+			ll tempx = from->x - cur.x;
+			tempx *= tempx;
+			ll tempy = from->y - cur.y;
+			tempy *= tempy;
+			d = min(d, tempy + tempx);
+		}
+		s.insert(cur);
+	}
+	cout << d << "\n";
+}
