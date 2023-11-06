bbd37827e5a6a37dd4df2a276490c345963fc8eb
keep1earning <linyihong1234@gmail.com>
2023-11-06 14:10:25 +0800

    Modifieed Graph Algorithms/Download_Speed.cpp Fixed the error in original solution, change the method of building graph

--- Graph Algorithms/Download_Speed.cpp
+++ Graph Algorithms/Download_Speed.cpp
@@ -3,24 +3,33 @@
 using namespace std;
 typedef long long ll;
 ll ans = 0;
+const int maxn = 500;
 int n;
-vector<vector<ll>> graph;
-vector<int> p;
-vector<bool> visited;
+
+struct edge{
+	int v;
+	ll w;
+	int rev_idx;
+};
+vector<edge> graph[maxn];
+int p[maxn], idx[maxn];
+bool visited[maxn];
 
 bool bfs(int node){
-	visited.assign(n, false);
+	memset(visited, 0, sizeof(visited));
 	queue<int> q;
 	q.push(node);
 	while (!q.empty()){
 		int f = q.front();
 		q.pop();
-		for (int i = 0; i < n; i++){
-			if (!graph[f][i] || visited[i])
+		for (int i = 0; i < graph[f].size(); i++){
+			int next = graph[f][i].v;
+			if (visited[next] || graph[f][i].w == 0)
 				continue;
-			visited[i] = true;
-			q.push(i);
-			p[i] = f;
+			visited[next] = true;
+			q.push(next);
+			p[next] = f;
+			idx[next] = i;
 		}
 	
 	}
@@ -28,27 +37,27 @@ bool bfs(int node){
 }
 
 int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
 	int m;
 	cin >> n >> m;
-	graph.assign(n, vector<ll>(n, 0));
-	p.resize(n);
-	visited.resize(n);
 	for (int i = 0; i < m; i++){
 		int u, v;
 		ll w;
 		cin >> u >> v >> w;
 		u--; v--;
-		graph[u][v] += w;
+		graph[u].push_back({v, w, (int)graph[v].size()});
+		graph[v].push_back({u, 0, (int)graph[u].size() - 1});
 	}
 	while (bfs(0)){
 		ll max_flow = 1e18;
 		for (int u = n - 1; u != 0; u = p[u]){
-			max_flow = min(max_flow, graph[p[u]][u]);
+			max_flow = min(max_flow, graph[p[u]][idx[u]].w);
 		}
 		ans += max_flow;
 		for (int u = n - 1; u != 0; u = p[u]){
-			graph[p[u]][u] -= max_flow;
-			graph[u][p[u]] += max_flow;
+			graph[p[u]][idx[u]].w -= max_flow;
+			graph[u][graph[p[u]][idx[u]].rev_idx].w += max_flow;
 		}
 	}
 	cout << ans << "\n";
