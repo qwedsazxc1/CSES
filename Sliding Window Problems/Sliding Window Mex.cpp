855b67461e9a82ea34db8815fcfd998dc8b6fcf7
keep1earning <linyihong1234@gmail.com>
2025-10-01 00:21:57 +0800

    Refactor Sliding Window Mex implementation for clarity and efficiency

--- Sliding Window Problems/Sliding Window Mex.cpp
+++ Sliding Window Problems/Sliding Window Mex.cpp
@@ -14,50 +14,81 @@ int main(){
         m[arr[i]]++;
     }
     set<pair<int, int>> s;
-    int prev = -1, l = -1;
+    int p = -1, l = -1;
     for (auto [x, y] : m){
-        if (prev == -1){
+        if (p == -1){
             l = x;
-            prev = x;
+            p = x;
             continue;
         }
-        if (x == prev + 1){
-            prev = x;
+        if (x == p + 1){
+            p = x;
             continue;
         }
-        s.insert({l, prev});
+        s.insert({l, p});
         l = x;
-        prev = x;
+        p = x;
     }
-    s.insert({l, prev});
+    s.insert({l, p});
     auto find = [&]() -> int {
         if (s.begin()->first != 0) return 0;
         return s.begin()->second + 1;
     };
 
-    cout << find() << "\n";
+    cout << find() << " \n"[k == n];
     for (int i = k; i < n; i++){
         if (arr[i] == arr[i - k]){
-            cout << find() << "\n";
+            cout << find() << " \n"[i == n - 1];
             continue;
         }
         m[arr[i]]++;
-        auto it = s.upper_bound({arr[i] + 1, -1});
-        if (it != s.begin()){
-            it--;
-            if (it->first <= arr[i] && arr[i] <= it->second){
-                pair<int, int> cur = *it;
-                s.erase(it);
-                if (cur.first == arr[i] && cur.second == arr[i]){
-                    // do nothing
-                } else if (cur.first == arr[i]){
-                    s.insert({cur.first + 1, cur.second});
-                } else if (cur.second == arr[i]){
-                    s.insert({cur.first, cur.second - 1});
-                } else {
-                    s.insert({cur.first, arr[i] - 1});
-                    s.insert({arr[i] + 1, cur.second});
+        if (m[arr[i]] == 1){
+            auto it = s.lower_bound({arr[i], arr[i]});
+            int l = arr[i], r = arr[i];
+            if (it != s.begin()){
+                auto it2 = prev(it);
+                if (it2->second + 1 == arr[i]){
+                    l = it2->first;
+                    s.erase(it2);
                 }
             }
+            if (it != s.end()){
+                if (it->first - 1 == arr[i]){
+                    r = it->second;
+                    s.erase(it);
+                }
+            }
+            s.insert({l, r});
+        }
+        m[arr[i - k]]--;
+        if (m[arr[i - k]] == 0){
+            auto it = s.lower_bound({arr[i - k], arr[i - k]});
+            int l, r;
+            if (it != s.end() && it->first == arr[i - k]){
+                l = it->first, r = it->second;
+                if (r == arr[i - k]){
+                    s.erase(it);
+                }
+                else{
+                    s.erase(it);
+                    s.insert({l + 1, r});
+                }
+                cout << find() << " \n"[i == n - 1];
+                continue;
+            }
+            it--;
+            l = it->first, r = it->second;
+            if (r == arr[i - k]){
+                s.erase(it);
+                s.insert({l, r - 1});
+            }
+            else{
+                s.erase(it);
+                s.insert({l, arr[i - k] - 1});
+                s.insert({arr[i - k] + 1, r});
+            }
+            
+        }
+        cout << find() << " \n"[i == n - 1];
     }
 }
