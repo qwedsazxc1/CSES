d640019f827d2660b1d18dac96c6b4c8aebdfcee
keep1earning <linyihong1234@gmail.com>
2025-11-14 17:23:38 +0800

    complete implementation for k-th highest score problem

--- Interactive Problems/K-th Highest Score.cpp
+++ Interactive Problems/K-th Highest Score.cpp
@@ -17,33 +17,41 @@ int main(){
 		m[{group, ord}] = res;
 		return res;
 	};
-	if (query(0, n) > query(1, 1) || query(1, n) > query(0, 1)){
-		int type = 0;
-		if (query(1, n) > query(0, 1))
-			type = 1;
-		if (k > n){
-			k -= n;
-			type ^= 1;
-		}
-		cout << "! " << query(type, k) << endl;
+	if (k <= n && query(0, k) > query(1, 1)){
+		cout << "! " << query(0, k) << endl;
+		return 0;
+	}
+	if (k <= n && query(1, k) > query(0, 1)){
+		cout << "! " << query(1, k) << endl;
+		return 0;
+	}
+	if (k - n > 0 && query(0, k - n) < query(1, n)){
+		cout << "! " << query(0, k - n) << endl;
+		return 0;
+	}
+	if (k - n > 0 && query(1, k - n) < query(0, n)){
+		cout << "! " << query(1, k - n) << endl;
 		return 0;
 	}
 	int r = min(n, k), l = max(1, k - n);
 	int res = r;
-	while (l < r){
+	while (l <= r){
 		int mid = (l + r) / 2;
 		int temp = k - mid;
-		if (query(0, mid) > query(1, temp)){
-			r = mid;
+		if (query(0, mid) < query(1, temp)){
+			r = mid - 1;
+			res = mid;
 		}
 		else{
-			res = mid;
 			l = mid + 1;
 		}
 	}
 	int ans = query(0, res);
-	if (query(0, res) > query(1, k - res + 1))
-		ans = query(1, k - res + 1);
+	int diff = query(1, k - res) - query(0, res);
+	if (k - res + 1 >= 1 && k - res + 1 <= n){
+		if (query(0, res) < query(1, k - res + 1))
+			ans = query(1, k - res + 1);
+	}
 	cout << "! " << ans << endl;
 
 }
