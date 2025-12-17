a007a5cd39e11c68b4c38425b97b69136c102fa7
keep1earning <linyihong1234@gmail.com>
2025-12-17 11:16:14 +0800

    fix implementation for bubble sort round

--- Additional Problems I/Bubble Sort Rounds II.cpp
+++ Additional Problems I/Bubble Sort Rounds II.cpp
@@ -3,30 +3,24 @@
 using namespace std;
 
 int main(){
-    int n, k;
-    cin >> n >> k;
-    vector<int> arr(n);
-    map<int, vector<int>> m;
-    for (int i = 0; i < n; i++){
-        cin >> arr[i];
-        m[arr[i]].push_back(i);
-    }
-    vector<int> ans(n);
-    vector<pair<int, int>> q;
-    for (int i = n - 1; i >= 0; i--){
-        if (!q.empty()){
-            ans[i] = q.back().second;
-            q.pop_back();
-            continue;
-        }
-        int l = i;
-        for (int j = 0; j < k && i - j >= 0; j++){
-            
-        }
-        
-        
-    }
-    for (int i = 0; i < n; i++){
-        cout << ans[i] << " \n"[i == n - 1];
-    }
+	int n, k;
+	cin >> n >> k;
+	vector<int> ans;
+	ans.reserve(n);
+	priority_queue<int, vector<int>, greater<>> pq;
+	for (int i = 0; i < n; i++){
+		int x;
+		cin >> x;
+		pq.push(x);
+		if (pq.size() > k){
+			ans.push_back(pq.top());
+			pq.pop();
+		}
+	}
+	while (pq.size()){
+		ans.push_back(pq.top());
+		pq.pop();
+	}
+	for (int i = 0; i < n; i++)
+		cout << ans[i] << " \n"[i == n - 1];
 }
